ff4719b60c37adb94b7e51745a0fa785b3a5ccec
ignacy <ignacy.buczek@onet.pl>
2017-08-11 11:07:06 +0200

    Added cookies

--- app/features/login/controllers/login.controller.js
+++ app/features/login/controllers/login.controller.js
@@ -8,7 +8,7 @@
  * Controller of the projectvocabularyAngularApp
  */
 angular.module('projectvocabularyAngularApp')
-  .controller('LoginCtrl', function ($scope, User) {
+  .controller('LoginCtrl', function ($scope, User, $cookies, $location) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,11 +16,30 @@ angular.module('projectvocabularyAngularApp')
     ];
     $scope.user = {
       email: "",
-      password: ""
+      password: "",
+      remember: false
+    }
+    if($cookies.get("email")){
+      var expireDate = new Date();
+      expireDate.setDate(expireDate.getDate()+1);
+      $cookies.put("email", $scope.user.email, {'expires': expireDate})
+      $location.path("/root");
     }
     $scope.submit = function(){
+      if($scope.remember){
+        console.log("dziala");
+        var expireDate = new Date();
+        expireDate.setDate(expireDate.getDate()+1);
+        console.log(expireDate);
+        $cookies.put("email", $scope.user.email, {'expires': expireDate});
+      }
       User.login($scope.user.email, $scope.user.password)
     }
+
+
+
+
+
 
 
   });
