f25e1c9362e553950dacc6cbbc956504fe86fa8b
ignacy <ignacy.buczek@onet.pl>
2017-11-16 15:02:00 +0100

    .

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopComponent } from './top/top.component';
 import { BottomComponent } from './bottom/bottom.component';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports:[
     TopComponent,
     BottomComponent,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   declarations: [TopComponent, BottomComponent]
 })
